9b3b1ff44de449817f5b899572125cc6da555bdb
sergiopgomez <55547267+sergiopgomez@users.noreply.github.com>
2020-05-06 10:37:21 +0200

    Add files via upload

---
+++ git_definitivo2/src/git_definitivo2/bienvenido.java
@@ -0,0 +1,10 @@
+package ejercicios;
+
+public class bienvenido {
+	  public static void main(String[] args) {
+		  
+	        String nombre="Fernando";
+	 
+	        System.out.println("Bienvenido "+nombre);
+	    }
+	}

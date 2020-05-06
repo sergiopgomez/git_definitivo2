8e6bcb374c0a9b5b52d115bcd04f12066a2ed566
sergiopgomez <55547267+sergiopgomez@users.noreply.github.com>
2020-05-06 10:41:01 +0200

    Add files via upload

---
+++ git_definitivo2/src/git_definitivo2/divisor.java
@@ -0,0 +1,15 @@
+package ejercicios;
+
+public class divisor {
+	  public static void main(String[] args) {
+		  
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Introduce un numero");
+	        int numero=sc.nextInt();
+	        if (numero%2==0){
+	            System.out.println("El numero "+numero+" es divisible entre 2");
+	        }else{
+	            System.out.println("El numero "+numero+" no es divisible entre 2");
+	        }
+	    }
+	    }

e01aa182983b829dc319f67667e235ca5f77b4fa
sergiopgomez <55547267+sergiopgomez@users.noreply.github.com>
2020-05-06 10:29:29 +0200

    Add files via upload

---
+++ git_definitivo2/src/git_definitivo2/operaciones.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+public class operaciones{
+		 
+	    public static void main(String[] args) {
+	        int num1=10;
+	        int num2=5;
+	        System.out.println("El resultado de la suma es "+(num1+num2));
+	 
+	        System.out.println("El resultado de la resta es "+(num1-num2));
+	 
+	        System.out.println("El resultado de la multiplicación es "+(num1*num2));
+	 
+	        System.out.println("El resultado de la división es "+(num1/num2));
+	    }
+	}
+
